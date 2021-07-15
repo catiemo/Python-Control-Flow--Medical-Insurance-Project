ea111ae9bf2da05a38187bded8cc37a3db6217f0
catiemo <87478282+catiemo@users.noreply.github.com>
2021-07-15 18:52:45 +0000

    Add files via upload

---
+++ Python Control Flow-Medical Insurance Project.py
@@ -0,0 +1,48 @@
+# Add your code here
+def analyze_smoker(smoker_status):
+  if smoker_status == 1:
+    print("To lower your cost, you should consider quitting smoking.")
+  else:
+    print("Smoking is not an issue for you.")
+  
+def analyze_bmi(bmi_value):
+  if bmi_value > 30:
+    print("Your BMI is in the obese range. To lower yoyr cost, you should significantly lower your BMI by " + str(bmi_value - 25) + " to bring it to a normal weight range.")
+  elif 25 <= bmi_value <= 30:
+      print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI by " + str(bmi_value - 25) + " to bring it to a weight normal range.")
+  elif (18.5 <= bmi_value < 25):
+      print("Your BMI is in a healthy range.")
+  else:
+      print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help you improve your health. You need to increase your BMI by " + str(18.5 - bmi_value) + " to bring it to a normal weight range.")
+
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  analyze_smoker(smoker)
+  analyze_bmi(bmi)
+  return estimated_cost
+ 
+# Estimate Keanu's insurance cost
+try:
+  keanu_insurance_cost = estimate_insurance_cost(name = "Keanu", age = 29, sex = 1, bmi = 26.2, num_of_children = 3, smoker = 1)
+except:
+  print("Please enter the right values of the parameters defined")
+
+# Estimate Nana's insurance cost
+try:
+  nana_insurance_cost = estimate_insurance_cost(name = "Nana", age = 39, sex = 1, bmi = 33, num_of_children = 2, smoker = 0)
+except:
+  print("Please enter the right values of the parameters defined")
+
+# Estimate Aba's insurance cost
+try:
+  aba_insurance_cost = estimate_insurance_cost(name = "Aba", age = 23, sex = 0, bmi = 20, num_of_children = 0, smoker = 0)
+except:
+  print("Please enter the right values of the parameters defined")
+
+# Estmate Yaw's insurance cost
+try:
+  yaw_insurance_cost = estimate_insurance_cost(name = "Yaw", age = 19, sex = 1, bmi = 17, num_of_children = 0, smoker = 1)
+except:
+  print("Please enter the right values of the parameters defined")
